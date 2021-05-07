8e454de0677e71bdeb9c8d9163960f4c399d0689
Ricardo Gusi <ricardogusi@hotmail.com>
2021-05-07 15:29:01 -0300

    problem on Netlify

--- frontend/src/utils/Requests.ts
+++ frontend/src/utils/Requests.ts
@@ -1,1 +1,3 @@
-export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "http://localhost:8081";
+export const BASE_URL = "https://sds3-gusi.herokuapp.com"
+
+//process.env.REACT_APP_BACKEND_URL ?? "http://localhost:8081";
